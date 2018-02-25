e3b5f6470722274a0000d24ecbe29e5470ec0dd4
parths3an <parth.93mukesh@gmail.com>
2018-02-24 16:18:00 -0800

    Working code

--- hello-world/src/components/loginComponents/generalForm.js
+++ hello-world/src/components/loginComponents/generalForm.js
@@ -2,17 +2,14 @@ import React, { Component } from 'react';
 
 
 class GeneralForm extends Component {
-fun = () =>
-{
-    <h1> okat this wirks</h1>
-}
+
   render()
  {
-this.fun;
+
     return (
       
       <div className="GeneralForm">
-        
+
 
         </div>
     );
